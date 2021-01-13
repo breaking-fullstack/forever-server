893b3223cc0941af7ef30968915ba4cee1c34c23
Faithfulness Alamu <vaguemail369@gmail.com>
2021-01-14 00:40:35 +0100

    Update music entity, add ID field

--- entity/music.go
+++ entity/music.go
@@ -1,6 +1,7 @@
 package entity
 
 type Music struct {
+	ID       string `json:"id"`
 	Title    string `json:"title" binding:"required"`
 	Artist   string `json:"artist"`
 	URL      string `json:"url" binding:"required,url"`
