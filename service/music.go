16950c67473fbfdd680e885b4fde784e0e74ba6b
Faithfulness Alamu <vaguemail369@gmail.com>
2021-01-14 17:53:23 +0100

    Create a music service implementation

--- service/music.go
+++ service/music.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 
+	"github.com/breaking-fullstack/forever-server/database"
 	"github.com/breaking-fullstack/forever-server/entity"
 )
 
@@ -10,4 +11,26 @@ type Music interface {
 	GetAll(ctx context.Context, userID string) ([]entity.Music, error)
 	Save(ctx context.Context, userID string, m entity.Music) error
 	Delete(ctx context.Context, userID string, mID string) error
+}
+
+type musicServiceImpl struct {
+	db database.DB
+}
+
+func NewMusic(db database.DB) Music {
+	return &musicServiceImpl{
+		db: db,
+	}
+}
+
+func (ms *musicServiceImpl) GetAll(ctx context.Context, userID string) ([]entity.Music, error) {
+	return ms.db.GetMusicList(ctx, userID)
+}
+
+func (ms *musicServiceImpl) Save(ctx context.Context, userID string, m entity.Music) error {
+	return ms.db.AddMusic(ctx, userID, m)
+}
+
+func (ms *musicServiceImpl) Delete(ctx context.Context, userID string, mID string) error {
+	return ms.db.RemoveMusic(ctx, userID, mID)
 }
