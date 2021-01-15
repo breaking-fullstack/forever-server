568e4737ba34fe1ddc2472013f81174663d9eb1d
Faithfulness Alamu <vaguemail369@gmail.com>
2021-01-15 16:57:05 +0100

    Omit thumburl field if empty in JSON

--- entity/music.go
+++ entity/music.go
@@ -5,5 +5,5 @@ type Music struct {
 	Title    string `json:"title" binding:"required"`
 	Artist   string `json:"artist"`
 	URL      string `json:"url" binding:"required,url"`
-	ThumbURL string `json:"thumb_url" binding:"url"`
+	ThumbURL string `json:"thumb_url" binding:"omitempty,url"`
 }
